15ee87d3e0b5f38654d41888786a62738d326116
gwyn <avalonvales@protonmail.com>
2022-09-27 21:39:20 -0300

    WIP dedicated function

--- quote to image/quote_to_image.py
+++ quote to image/quote_to_image.py
@@ -4,14 +4,22 @@ import csv
 jobs = 50
 imagenumber = 0
 previoustime = ''
+color_bg = (255,255,255)   # white
+color_norm = (125,125,125) # grey
+color_high = (0,0,0)       # black
+# fontszie_credit = 18
+# font_credit = 'baskervilleboldbt.ttf'
+# font_norm = 'bookerly.ttf'
+# font_high = 'bookerlybold.ttf'
+# def add_credits(drawobj, author:str, title:str):
 
-def TurnQuoteIntoImage(time, quote, timestring, fname=None, title=None, author=None):
+def TurnQuoteIntoImage(time:str, quote:str, timestring:str, author:str, title:str):
     global imagenumber, previoustime
     imagesize = (600,800)
     quoteheight = 745
-    quotelength = 570
+    quotelength = 575
     v_anchor = 0
-    h_anchor = 18
+    h_anchor = 14
     c_fnt1 = (125,125,125) # grey
     c_fnt2 = (0,0,0)       # black
     c_bg = (255,255,255)   # white
@@ -21,42 +29,56 @@ def TurnQuoteIntoImage(time, quote, timestring, fname=None, title=None, author=N
     ariandel = Image.new(mode='RGB', size=(imagesize), color=c_bg)
     paintedworld = ImageDraw.Draw(ariandel)
 
+    attribution = f'—{title}, {author}'
+
     if time == previoustime:
         imagenumber += 1
     else:
         imagenumber = 0
         previoustime = time
-    savepath = f'images/test2/quote_{time}_{fname}.png'
-
-    # the case and timestring can be mismatched. Checking this is very important
-    timestr_starts = 0
-    try:
-        timestr_starts = quote.lower().index(timestring.lower())
-    except ValueError:
-        print(f'WARNING: missing timestring for {savepath}, discarding')
-        return
-
-    timestr_ends = timestr_starts + len(timestring)
+    savepath = f'images/test/quote_{time}_{author}.png'
 
     quote, fntsize = calc_fntsize(quotelength, quoteheight, quote, fntname1)
     fnt1 = ImageFont.FreeTypeFont(fntname1, fntsize)
     fnt2 = ImageFont.FreeTypeFont(fntname2, fntsize)
+    paintedworld = highlight_substr(paintedworld, (h_anchor,v_anchor), quote, timestring, fnt1, fnt2)
 
-    # sandwich timestring in heads so it's easy to to find later
-    head = '<|*_*|>'
-    lines = quote[:timestr_starts]
-    lines += f'{head}{quote[timestr_starts:timestr_ends]}{head}'
-    lines += quote[timestr_ends:]
+    if not paintedworld == None:
+        ariandel.save(savepath)
+    # ariandel.show()
 
-    g_fnt = fnt1
-    g_color = c_fnt1
+def highlight_substr(drawobj, anchors:tuple, text:str, substr:str,
+        font_norm: ImageFont.FreeTypeFont, font_high: ImageFont.FreeTypeFont):
+    # all this function does is add text to the drawobj with the substr
+    # highlighted. it doesn't check if it will fit the image or anything else
+    h_anchor = anchors[0]
+    v_anchor = anchors[1]
+
+    # the case for the substr can be mismatched. Checking this is very important
+    substr_starts = 0
+    try:
+        substr_starts = text.lower().index(substr.lower())
+    except ValueError:
+        return None
+    substr_ends = substr_starts + len(substr)
+    head = '|'
+    lines = text[:substr_starts]
+    lines += f'{head}{text[substr_starts:substr_ends]}{head}'
+    lines += text[substr_ends:]
+
+    style_norm = (color_norm, font_norm)
+    style_high = (color_high, font_high)
+    current_style = style_norm
+    heads_found = 0
+    write = drawobj.text
+    textlength = drawobj.textlength
+    # wordnow = ''
     h_pos = h_anchor
     v_pos = v_anchor
-    heads_found = 0
-    in_timestring = False
-    write = paintedworld.text
-    textlength = paintedworld.textlength
-    wordnow = ''
+    # this would be a LOT simpler if we didn't have to check the edges attached
+    # to the substring. it might be easier to implement a char by char loop
+    # in the future, using the kerning calculation method in this example:
+    # https://pillow.readthedocs.io/en/stable/reference/ImageFont.html#PIL.ImageFont.FreeTypeFont.getlength
     for line in lines.splitlines():
         for word in line.split():
             word += ' '
@@ -64,60 +86,47 @@ def TurnQuoteIntoImage(time, quote, timestring, fname=None, title=None, author=N
             # bits stuck to it, and print the whole thing in 3 parts
             if word.count(head) == 2:
                 wordnow = word.split(head)[0]
-                write((h_pos, v_pos), wordnow, c_fnt1, font=fnt1)
-                h_pos += textlength(wordnow, fnt1)
+                write((h_pos, v_pos), wordnow, *style_norm)
+                h_pos += textlength(wordnow, font_norm)
                 wordnow = word.split(head)[1]
-                write((h_pos, v_pos), wordnow, c_fnt2, font=fnt2)
-                h_pos += textlength(wordnow, fnt2)
+                write((h_pos, v_pos), wordnow, *style_high)
+                h_pos += textlength(wordnow, font_high)
                 wordnow = word.split(head)[2]
-                write((h_pos, v_pos), wordnow, c_fnt1, font=fnt1)
-                h_pos += textlength(wordnow, fnt1)
+                write((h_pos, v_pos), wordnow, *style_norm)
+                h_pos += textlength(wordnow, font_norm)
                 word = ''
-                heads_found += 2
-            # otherwise change the font, and wait for the next head
+            # otherwise change the default font, and wait for the next head
             elif word.count(head) == 1:
-                if heads_found == 0:
-                    in_timestring = True
-                    g_fnt = fnt2
-                    g_color = c_fnt2
-                    wordnow = word.split(head)[0]
-                    word = word.split(head)[1]
-                if heads_found == 1:
-                    in_timestring = False
-                    wordnow = word.split(head)[0]
-                    word = word.split(head)[1]
-                write((h_pos, v_pos), wordnow, c_fnt2, font=fnt2)
-                h_pos += textlength(wordnow, g_fnt)
                 heads_found += 1
-            if in_timestring:
-                g_fnt = fnt2
-                g_color = c_fnt2
-            elif not in_timestring:
-                g_fnt = fnt1
-                g_color = c_fnt1
+                wordnow = word.split(head)[0]
+                word = word.split(head)[1]
+                write((h_pos, v_pos), wordnow, *style_high)
+                h_pos += textlength(wordnow, font_high)
+                if heads_found == 1:
+                    current_style = style_high
+                else:# if heads_found == 1:
+                    current_style = style_norm
             # this is the bit that actually does most of the writing
-            write((h_pos, v_pos), word, g_color, font=g_fnt)
-            h_pos += textlength(word, g_fnt)
+            write((h_pos, v_pos), word, *current_style)
+            h_pos += textlength(word, current_style[1])
         # the offset calculated by multiline_text (what we're trying to mimic)
         # is based on uppercase letter A plus 4 pixels for whatever fucking
         # reason. see: https://github.com/python-pillow/Pillow/discussions/6620
-        v_pos += fnt1.getbbox("A")[3] + 4
+        v_pos += font_norm.getbbox("A")[3] + 4
         h_pos = h_anchor
 
-    ariandel.save(savepath)
-    # ariandel.show()
+    return drawobj
 
 
-def wrap_lines(text:str, font:ImageFont.FreeTypeFont, line_length:int,
-                                                         strict=False):
+def wrap_lines(text:str, font:ImageFont.FreeTypeFont, line_length:int):
     # wraps lines to maximize the number of words within line_length. note
-    # that long words or a big font can easily exceed line_length, you still
-    # need to check the bbox size elsewhere. adapted from Chris Collett
+    # that lines *can* exceed line_length, this is intentional, as text looks
+    # better if the font is rescaled afterwards. adapted from Chris Collett
     # https://stackoverflow.com/a/67203353/8225672
         lines = ['']
         for word in text.split():
             line = f'{lines[-1]} {word}'.strip()
-            if font.getlength(line) < line_length:
+            if font.getlength(line) <= line_length:
                 lines[-1] = line
             else:
                 lines.append(word)
@@ -159,13 +168,18 @@ def calc_fntsize(length:int, height:int, text:str, fntname:str, basesize=2,
         boxlength = monalisa.multiline_textbbox((0,0), lines, fnt)[2]
     return lines, fntsize
 
-with open('litclock_annotated_br2.csv', newline='\n') as csvfile:
-    quotereader = csv.DictReader(csvfile, delimiter='|')
-    i = 0
-    for row in quotereader:
-        if i >= jobs:
-        # if i >= 100:
-            break
-        else:
-            i += 1
-            TurnQuoteIntoImage(row['time'],row['quote'], row['timestring'], row['author'])
+if __name__ == '__main__':
+    with open('litclock_annotated_br2.csv', newline='\n') as csvfile:
+        quotereader = csv.DictReader(csvfile, delimiter='|')
+        i = 0
+        for row in quotereader:
+            if i >= jobs:
+            # if i >= 100:
+                break
+            else:
+                i += 1
+                TurnQuoteIntoImage(row['time'],row['quote'], row['timestring'], row['author'], row['title'])
+    # text = "Midnight, and there was frost on the fields and patches of black ice on a road notorious for its accidents—there were bunches of dead flowers tied to fence posts and telegraph poles, and torn gaps in hedges, like some kind of failed crop."
+    # font = ImageFont.FreeTypeFont('bookerly.ttf', 53)
+    # print("...\n\n\n...")
+    # print(wrap_lines(text, font, 570))
